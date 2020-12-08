a9879d11ef55a712b35987f733410ad480e302bb
henettaja <henri.vaisane@gmail.com>
2020-12-08 15:08:35 +0200

    Attempt at using firebase for db

--- utilities.js
+++ utilities.js
@@ -1,5 +1,10 @@
+import {DateTime} from "luxon";
 
 export default function valuesToPercentage(target, current) {
 
     return 100 * (current / target)
+}
+
+export function today() {
+    return DateTime.local().toSQLDate();
 }
