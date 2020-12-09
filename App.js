8679e3cb8860bbe05f4e53ce272334b05c8595f3
henettaja <henri.vaisane@gmail.com>
2020-12-09 13:26:53 +0200

    Fix target dialog type error

--- App.js
+++ App.js
@@ -5,6 +5,22 @@ import {DefaultTheme, Provider as PaperProvider} from "react-native-paper";
 import { StyleSheet, View } from 'react-native';
 import firebase from "firebase/app";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyDDJdE1_PFukKC53IoewJ9aK5zt83Ei1ao",
+    authDomain: "water-tracker-us.firebaseapp.com",
+    databaseURL: "https://water-tracker-us-default-rtdb.firebaseio.com/",
+    projectId: "water-tracker-us",
+    storageBucket: "water-tracker-us.appspot.com",
+    messagingSenderId: "225268416727",
+    appId: "1:225268416727:web:01c0005c7f8f4048637092"
+};
+
+if (firebase.apps.length === 0) {
+    firebase.initializeApp(firebaseConfig);
+}else {
+    firebase.app(); // if already initialized, use that one
+}
+
 export default function App() {
 
     const theme = {
