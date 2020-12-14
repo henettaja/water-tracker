90430470c46d31fe1871872054e70da2c4dbfe52
henettaja <henri.vaisane@gmail.com>
2020-12-14 20:22:25 +0200

    Settings dialog now shows current volumes

--- components/SettingsDialog.js
+++ components/SettingsDialog.js
@@ -24,6 +24,15 @@ export default function SettingsDialog(props) {
         ).then(r => null);
     }
 
+    React.useEffect(() => {
+        firebase.database().ref('containers/001/').on('value', snapshot => {
+            const data = snapshot.val();
+            const prods = Object.values(data);
+            setBottleVolume(prods[0]);
+            setCupVolume(prods[1]);
+        })
+    })
+
     return (
         <Provider>
             <Portal>
@@ -34,6 +43,14 @@ export default function SettingsDialog(props) {
                     <Dialog.Content style={{alignContent: 'space-around'}}>
                         <Text>Here you can customize the volumes of the water cup and bottle
                             to match your own to make adding water quick and easy.</Text>
+
+                        <Text>{`
+Current volumes:
+
+Water cup: ${cupVolume} ml
+Water bottle: ${bottleVolume} ml`
+                        }</Text>
+
                         <TextInput
                             style={{marginTop: 20}}
                             label="Volume of your water cup"
