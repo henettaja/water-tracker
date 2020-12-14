dd83840e7ac8a4bab64e7225a52f17afb71e9dc4
henettaja <henri.vaisane@gmail.com>
2020-12-14 19:00:13 +0200

    Changed history screen icon

--- components/Bottomnav.js
+++ components/Bottomnav.js
@@ -11,7 +11,7 @@ const Bottomnav = () => {
     const [index, setIndex] = React.useState(0);
     const [routes] = React.useState([
         { key: 'main', title: 'Main', icon: 'home' },
-        { key: 'history', title: 'History', icon: 'history' },
+        { key: 'history', title: 'History', icon: 'chart-bell-curve' },
     ]);
 
     const renderScene = BottomNavigation.SceneMap({
