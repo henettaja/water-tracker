f8c6d164aa8e7ecb8aa1fce445a4546049fd40fe
henettaja <henri.vaisane@gmail.com>
2020-12-17 13:28:54 +0200

    Clean commit

---
+++ App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import Bottomnav from "./components/Bottomnav"
+import {DefaultTheme, Provider as PaperProvider} from "react-native-paper";
+import { StyleSheet, View } from 'react-native';
+import firebase from "firebase/app";
+import TopBar from "./components/TopBar";
+import {firebaseConfig} from "./firebaseConfig";
+
+if (firebase.apps.length === 0) {
+    firebase.initializeApp(firebaseConfig);
+}else {
+    firebase.app(); // if already initialized, use that one
+}
+
+export default function App() {
+
+    const theme = {
+        ...DefaultTheme,
+        dark: true,
+        mode: 'adaptive',
+        roundness: 15,
+        colors: {
+            ...DefaultTheme.colors,
+            primary: '#2176FF',
+            accent: '#33A1FD',
+            surface: '#131A26',
+            background: '#131A26',
+            text: '#FFFFFF'
+        },
+    };
+
+    return (
+        <PaperProvider theme={theme}>
+            <View style={styles.container}>
+                <View style={styles.content}>
+                    <TopBar/>
+                    <Bottomnav/>
+                </View>
+                <StatusBar style="auto" />
+            </View>
+        </PaperProvider>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+    },
+    content: {
+        flex: 10,
+    }
+});
